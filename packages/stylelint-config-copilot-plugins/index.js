4ca9f3b35441390cf114486bfcd6c3e76e6015d5
Florian Uhlig <florianuhlig@outlook.com>
2019-06-01 21:48:32 +0200

    [FEATURE] Plugins: Add rule to prevent low-performance animations
    - `stylelint-high-performance-animation`

--- packages/stylelint-config-copilot-plugins/index.js
+++ packages/stylelint-config-copilot-plugins/index.js
@@ -4,16 +4,19 @@
  */
 module.exports = {
   'plugins': [
-    'stylelint-declaration-block-no-ignored-properties'
+    'stylelint-declaration-block-no-ignored-properties',
+    'stylelint-high-performance-animation'
   ],
   'rules': {
     'plugin/declaration-block-no-ignored-properties': true,
-    // 'plugin/no-low-performance-animation-properties': [
-    //   true,
-    //   {
-    //     ignore: 'paint-properties'
-    //   }
-    // ],
+
+    // Stylelint rule for preventing the use of low performance animation and transition properties.
+    'plugin/no-low-performance-animation-properties': [
+      true,
+      {
+        ignore: 'paint-properties'
+      }
+    ],
 
     // 'sh-waqar/declaration-use-variable': [
     //   [
