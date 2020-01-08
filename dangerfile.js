344eab887ea41e586d52aaad8396de8f9dfe1183
Florian Uhlig <florianuhlig@outlook.com>
2020-01-08 15:50:29 +0100

    refactor(danger): reorganize imports
    fix(dangerfile): import syntax

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,9 @@
-const danger = require('danger');
+import { danger, warn } from 'danger';
 const _ = require('lodash');
 
 const hasPackageChanges = _.includes(danger.git.modified_files, 'package.json');
 const hasLockfileChanges = _.includes(danger.git.modified_files, 'package-lock.json');
+
 if (hasPackageChanges && !hasLockfileChanges) {
-  danger.warn('There are `package.json` changes with no corresponding lockfile changes');
+  warn('There are package.json changes with no corresponding lockfile changes');
 }
