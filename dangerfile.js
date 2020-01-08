c2c5d604fc5f6c544ef56cf663a966543e7da656
Florian Uhlig <florianuhlig@outlook.com>
2020-01-08 10:57:01 +0100

    ci!: add danger to enforce ci/PR rules
    - add danger file with boilerplate code
    - add danger step in ci (circle)
    
    BREAKING-CHANGE:
    updated dependency, run `npm install`
    
    ci(test): fail instead of warn if rule is not applied

---
+++ dangerfile.js
@@ -0,0 +1,6 @@
+import { danger, fail } from 'danger';
+
+// No PR is too small to include a description of why you made a change
+if (danger.github.pr.body.length < 10) {
+  fail('Please include a description of your PR changes.');
+}
