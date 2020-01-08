26d8112cc911def086474e120970f95b5611193b
Florian Uhlig <florianuhlig@outlook.com>
2020-01-08 17:19:45 +0100

    ci(danger): add rules for dependency handling
    - warn if there are updates to the dependencies, ensure marking as breaking change
    - fail if there are updates to the dependencies without corresponding udpate to lock-file

--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,37 @@
-import { danger, warn } from 'danger';
+import { danger, warn, fail, schedule } from 'danger';
 const _ = require('lodash');
 
-const hasPackageChanges = _.includes(danger.git.modified_files, 'package.json');
-const hasLockfileChanges = _.includes(danger.git.modified_files, 'package-lock.json');
+/**
+ * Check if `package.json` has updated dependencies (added, removed, updated).
+ * @param {Object} diff diff-object of `package.json`
+ */
+function hasUpdatedDependencies(diff) {
+  if (diff.devDependencies) {
+    return !!(diff.devDependencies.after);
+  }
 
-if (hasPackageChanges && !hasLockfileChanges) {
-  warn('There are package.json changes with no corresponding lockfile changes');
+  if (diff.dependencies) {
+    return !!(diff.devDependencies.after);
+  }
+
+  return false;
 }
+
+/**
+ * Check if there was an update to the `package-lock.json`.
+ */
+function hasUpdatedLockfile() {
+  return _.includes(danger.git.modified_files, 'package-lock.json');
+}
+
+schedule(async() => {
+  const packageDiff = await danger.git.JSONDiffForFile('package.json');
+
+  if (hasUpdatedDependencies(packageDiff)) {
+    warn('Updated dependencies, did you mark it as a breaking change?');
+
+    if (!hasUpdatedLockfile()) {
+      fail('There are changes in `package.json` with no corresponding changes in `package-lock.json`.\n Make sure to run `npm install` to update modules and lock-file.');
+    }
+  }
+});
