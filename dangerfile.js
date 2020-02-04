16d1595e5cbb1b4f3051883819c7f1ae7233aee7
Florian Uhlig <florianuhlig@outlook.com>
2020-02-04 22:01:56 +0100

    refactor(danger): use es6 import

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,29 @@
 import { danger, warn, fail, schedule } from 'danger';
-const _ = require('lodash');
+import { includes } from 'lodash';
+
+const COMMITS = danger.git.commits;
+const FILES_MODIFIED = danger.git.modified_files;
+
+COMMITS.forEach(commit => {
+  const message = commit.message;
+  const breakingChange = {
+    indicator: '!:',
+    title: 'BREAKING CHANGE',
+    reference: 'See [conventional commits](https://www.conventionalcommits.org/en/v1.0.0/#commit-message-with-both-and-breaking-change-footer) for details.',
+  };
+
+  if (message.includes(breakingChange.indicator)) {
+    if (!message.includes(breakingChange.title)) {
+      warn(`The commit message title indicates a breaking change. Make sure to include a "BREAKING CHANGE" in the commit footer to give more details about the breaking change. ${breakingChange.reference}`);
+    }
+  }
+
+  if (message.includes(breakingChange.title)) {
+    if (!message.includes(breakingChange.indicator)) {
+      warn(`The commit includes a breaking change without an indicator in the title ("<type>!: <title>"). Make sure to include it to draw attention to the breaking change. See [conventional commits](https://www.conventionalcommits.org/en/v1.0.0/#commit-message-with-both-and-breaking-change-footer) for details. ${breakingChange.reference}`);
+    }
+  }
+});
 
 /**
  * Check if `package.json` has updated dependencies (added, removed, updated).
@@ -21,7 +45,7 @@ function hasUpdatedDependencies(diff) {
  * Check if there was an update to the `package-lock.json`.
  */
 function hasUpdatedLockfile() {
-  return _.includes(danger.git.modified_files, 'package-lock.json');
+  return includes(FILES_MODIFIED, 'package-lock.json');
 }
 
 schedule(async() => {
