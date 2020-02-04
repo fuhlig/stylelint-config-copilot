3bd72347f954c79e0b40df23c410dfdbfb1cfb32
fuhlig <fuhlig@users.noreply.github.com>
2020-02-04 23:49:04 +0100

    97/chore/danger commit msg breaking change (#107)
    * chore(greenkeeper): modifiy commit messages to match guidelines (#97)
    
    - mark dependency updates as breaking changes
    
    * chore(deps)!: add babel
    
    BREAKING-CHANGE:
    Updated dependencies, run `npm install`
    
    * refactor(danger): use es6 import
    
    * fix: breaking-change title
    
    * fix: remove warning
    
    - remove warning, is replaced with commit check

--- dangerfile.js
+++ dangerfile.js
@@ -1,5 +1,31 @@
 import { danger, warn, fail, schedule } from 'danger';
-const _ = require('lodash');
+import { includes } from 'lodash';
+
+const COMMITS = danger.git.commits;
+const FILES_MODIFIED = danger.git.modified_files;
+
+COMMITS.forEach(commit => {
+  const message = commit.message;
+  const breakingChange = {
+    indicator: '!:',
+    title: 'BREAKING-CHANGE',
+    reference: 'See [conventional commits](https://www.conventionalcommits.org/en/v1.0.0/#commit-message-with-both-and-breaking-change-footer) for details.',
+  };
+
+  if (message.includes(breakingChange.indicator)) {
+    if (!message.includes(breakingChange.title)) {
+      warn(`The commit message title indicates a breaking change. Make sure to include a "BREAKING CHANGE" in the commit footer to give more details about the breaking change. ${breakingChange.reference}`);
+      warn(message);
+    }
+  }
+
+  if (message.includes(breakingChange.title)) {
+    if (!message.includes(breakingChange.indicator)) {
+      warn(`The commit includes a breaking change without an indicator in the title ("<type>!: <title>"). Make sure to include it to draw attention to the breaking change. See [conventional commits](https://www.conventionalcommits.org/en/v1.0.0/#commit-message-with-both-and-breaking-change-footer) for details. ${breakingChange.reference}`);
+      warn(message);
+    }
+  }
+});
 
 /**
  * Check if `package.json` has updated dependencies (added, removed, updated).
@@ -21,15 +47,13 @@ function hasUpdatedDependencies(diff) {
  * Check if there was an update to the `package-lock.json`.
  */
 function hasUpdatedLockfile() {
-  return _.includes(danger.git.modified_files, 'package-lock.json');
+  return includes(FILES_MODIFIED, 'package-lock.json');
 }
 
 schedule(async() => {
   const packageDiff = await danger.git.JSONDiffForFile('package.json');
 
   if (hasUpdatedDependencies(packageDiff)) {
-    warn('Updated dependencies, did you mark it as a breaking change?');
-
     if (!hasUpdatedLockfile()) {
       fail('There are changes in `package.json` with no corresponding changes in `package-lock.json`.\n Make sure to run `npm install` to update modules and lock-file.');
     }
