a53ac334acc740c4640b91a10b1464908fc36ae7
Florian Uhlig <florianuhlig@outlook.com>
2020-10-24 23:26:34 +0200

    build(config): add root-config
    - track file via git
    - allows testing root-config

---
+++ .stylelintrc.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  plugins: [
+  ],
+  extends: [
+    './packages/stylelint-config-copilot-base',
+    './packages/stylelint-config-copilot-scss',
+    './packages/stylelint-config-copilot-plugins',
+    './packages/stylelint-config-copilot-order',
+  ],
+  ignoreFiles: [
+    // enter files to ignore (e.g. libs, styleguide styles)
+    '**/node_modules/**',
+  ],
+  defaultSeverity: 'warning',
+  rules: {
+    // custom-rules & overwrites
+  },
+};
